823048f081d801c3625c1caea4af6f29851b7815
tominay18 <92387353+tominay18@users.noreply.github.com>
2021-10-14 21:14:11 +0300

    HomeWork-2

---
+++ main.js
@@ -0,0 +1,61 @@
+let player1 = {
+    name: 'Scorpion',
+    hp: 70,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['Кунай', 'Меч', 'Ниндзято'],
+    attack: function() {
+        console.log (this.name + ' Fight...');
+    }
+}
+
+
+
+let player2 = {
+    name: 'Kitana',
+    hp: 55,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+    weapon: ['Стальные веера', 'Летящий клинок'],
+    attack: function() {
+        console.log (this.name + ' Fight...');
+    }
+}
+
+player1.attack();
+player2.attack();
+
+function createPlayer(playerClass, playerName, hp) {
+    const $player = document.createElement('div');
+    $player.classList.add(playerClass);
+
+    const $progressbar = document.createElement('div');
+    $progressbar.classList.add('progressbar');
+
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.style.width = playerName + '%';
+
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = playerName.name;
+
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+
+    const $img = document.createElement('img');
+    $img.src = playerName.img;
+
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+    $progressbar.appendChild($life);
+    $progressbar.appendChild($name);
+    $character.appendChild($img);
+
+    const arenas = document.querySelector('.arenas');
+    arenas.appendChild($player);
+
+
+}
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
+
